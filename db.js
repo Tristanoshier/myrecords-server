c06d8e095d7d8b876028776a8d4b43b8f1c68792
Tristanoshier <tsoshier@bsu.edu>
2020-03-12 10:30:40 -0400

    deployment

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_NAME, 'postgres', process.env.PASSWORD, {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres'
 });
 
